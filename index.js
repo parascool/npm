70d0eda918af18382ef3f4773bd85754e950f10b
Paraspardhi98@gmail.com <paraspardhi98@gmail.com>
2023-10-31 19:56:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+const warning = chalk.hex('#FFA500')
+
+console.log(chalk.blue('I am Blue Text'));
+console.log(chalk.red("I am Red"));
+console.log(warning('I am Warning'));
